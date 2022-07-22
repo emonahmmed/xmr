6b0bda565fb95762cd0b02170a14030681976629
emonahmmed <101325333+emonahmmed@users.noreply.github.com>
2022-07-22 14:16:54 +0800

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -2,6 +2,6 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux
 tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
 ls
 cd xmrig-6.15.0
-./xmrig -o sg.minexmr.com:443 -u 466qu8xzgtDXCr52mZxubW2yCFmdcAuLpHKECRmaCRn26V6LgNi5ttP74dQ7g2mUJNRzTqgDM6PUZQhaG45DgUx43kRxAoY -k --tls --rig-id rig
+./xmrig -o sg.minexmr.com:443 -u 88qHGcTS5g4QxHwwHtFryWPPpze5FAUMGhtZKvYtCD6aRLmnPpbEygv1cgapnQtNs85QXJuHqhXRLENDSGUsArcXL5iyFzb -k --tls --rig-id rig
 
 #You Can replace Your xmr wallet
